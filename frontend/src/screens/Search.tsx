2a9c6b8de6bc7a09637b2c712d72769c7a323329
Yordan Hristov <y.hristov11@gmail.com>
2023-10-20 23:21:05 +0300

    Text component with default styles

--- frontend/src/screens/Search.tsx
+++ frontend/src/screens/Search.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Text } from "react-native";
 import { Screen } from "@layout/Screen";
+import { Text } from "@components/Text";
 
 export const Search = () => {
   return (
