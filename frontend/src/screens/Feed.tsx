b4d8accd0a1d6978391283ff019cc49f12cd40a5
Yordan Hristov <y.hristov11@gmail.com>
2023-10-21 23:15:21 +0300

    Initial Feed page

--- frontend/src/screens/Feed.tsx
+++ frontend/src/screens/Feed.tsx
@@ -1,11 +1,38 @@
 import React from "react";
+import { Dimensions, FlatList, Text, View } from "react-native";
 import { Screen } from "@layout/Screen";
-import { Text } from "@components/Text";
+import { Fact } from "@features/facts";
+
+import data from "../../data.json";
+import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
+import { useSafeAreaFrame } from "react-native-safe-area-context";
 
 export const Feed = () => {
+  const bottomBarHeight = useBottomTabBarHeight();
+  const { height } = useSafeAreaFrame();
+
   return (
     <Screen>
-      <Text>Feed Screen</Text>
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        snapToAlignment="start"
+        decelerationRate="fast"
+        snapToInterval={height - bottomBarHeight - 50}
+        data={data}
+        renderItem={({ item }) => (
+          <View
+            style={{
+              height: height - bottomBarHeight - 50,
+            }}
+          >
+            <Fact
+              title={item.title}
+              summary={item.summary}
+              source={item.source}
+            />
+          </View>
+        )}
+      />
     </Screen>
   );
 };
