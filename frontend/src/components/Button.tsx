824f6154abd2cab0f0382c34128bcbc5ad647798
Yordan Hristov <y.hristov11@gmail.com>
2023-10-26 23:39:45 +0300

    Extend Button component with type and icon props

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,28 +1,28 @@
-import React from "react";
-import {
-  Button as ReactNativeButton,
-  ButtonProps,
-  StyleSheet,
-  View,
-  Pressable,
-  PressableProps,
-} from "react-native";
+import React, { ReactNode } from "react";
+import { StyleSheet, View, Pressable, PressableProps } from "react-native";
 import { Text } from "./Text";
 import theme from "theme";
 
 interface Props extends PressableProps {
   title: string;
+  type?: "primary" | "secondary";
+  icon?: ReactNode;
 }
 
 export const Button = (props: Props) => {
-  const { title, ...rest } = props;
+  const { title, type = "primary", icon, ...rest } = props;
 
   return (
-    <View style={styles.button}>
-      <Pressable {...rest}>
+    <Pressable
+      style={({ pressed }) => [{ opacity: pressed ? 0.5 : 1 }]}
+      {...rest}
+    >
+      <View style={[styles[type], !!icon && styles.withIcon]}>
+        {icon}
+
         <Text style={styles.text}>{title}</Text>
-      </Pressable>
-    </View>
+      </View>
+    </Pressable>
   );
 };
 
@@ -31,14 +31,20 @@ Button.defaultProps = {
 };
 
 const styles = StyleSheet.create({
-  button: {
+  withIcon: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  primary: {
     borderWidth: theme.borderWidth,
     borderColor: theme.white,
     borderRadius: theme.borderRadius,
   },
+  secondary: {},
   text: {
     paddingVertical: 7,
     textAlign: "center",
-    fontSize: 16
+    fontSize: 16,
   },
 });
