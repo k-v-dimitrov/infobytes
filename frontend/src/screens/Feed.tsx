7d20faf56a5df098d7ee8366395b09db31d500ba
Yordan Hristov <y.hristov11@gmail.com>
2023-10-26 23:43:10 +0300

    Add clear filter option to Feed screen

--- frontend/src/screens/Feed.tsx
+++ frontend/src/screens/Feed.tsx
@@ -4,7 +4,8 @@ import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
 import { Screen } from "@layout/Screen";
 import { Fact, useGetUserFeed } from "@features/feed";
-import { RootStackParamList, Screens } from "@utils/navigation";
+import { RootStackParamList, Screens, useNavigation } from "@utils/navigation";
+import { Button, Icon, Text } from "components";
 
 export const Feed = ({
   route,
@@ -12,16 +13,37 @@ export const Feed = ({
   const bottomBarHeight = useBottomTabBarHeight();
   const height = Dimensions.get("window").height;
   const { loading, facts, getUserFeed } = useGetUserFeed();
+  const navigation = useNavigation();
   const { category } = route.params || {};
 
-  const HEIGHT = height - bottomBarHeight - (Platform.OS === "ios" ? 50 : 0);
+  // temporary solution
+  const HEIGHT =
+    height -
+    bottomBarHeight -
+    (Platform.OS === "ios" ? 50 : 0) -
+    (!!category ? 30 : 0);
 
   useEffect(() => {
     getUserFeed(category);
   }, [category]);
 
+  const clearFilter = () => {
+    navigation.navigate(Screens.FEED);
+  };
+
   return (
     <Screen loading={loading}>
+      {category && (
+        <Button
+          style={{ alignSelf: "flex-start", width: "25%" }}
+          icon={<Icon name="close-outline" size={24} />}
+          onPress={clearFilter}
+          title={category}
+        />
+      )}
+
+      {facts.length === 0 && <Text>No facts found...</Text>}
+
       <FlatList
         showsVerticalScrollIndicator={false}
         snapToAlignment="start"
