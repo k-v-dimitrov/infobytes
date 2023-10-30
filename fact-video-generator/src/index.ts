e19b83c026e8b50f2c88860ce7a7e02bf4c3bdb8
Kiril Dimitrov <kir4agi@gmail.com>
2023-10-30 18:41:36 +0200

    feat: successful select of all facts

--- fact-video-generator/src/index.ts
+++ fact-video-generator/src/index.ts
@@ -1,75 +1,36 @@
-import * as fs from 'fs';
+import knex from 'knex';
+
 import dotenv from 'dotenv';
-import { renderFrame } from './modules/frame-renderer';
-import { getAudioFileLength } from './utils';
 dotenv.config();
 
-const OUTPUT_DIR = './frames';
+// const {
+//   FPS: configFPS,
+//   VIDEO_WIDTH: configVideoWidth,
+//   VIDEO_HEIGHT: configVideoHeight,
+// } = process.env;
 
-const {
-  FPS: configFPS,
-  VIDEO_WIDTH: configVideoWidth,
-  VIDEO_HEIGHT: configVideoHeight,
-} = process.env;
+// const FPS = Number.parseInt(configFPS);
+// const VIDEO_WIDTH = Number.parseInt(configVideoWidth);
+// const VIDEO_HEIGHT = Number.parseInt(configVideoHeight);
+// const FRAMES_OUTPUT_DIR = './frames';
 
-const FPS = Number.parseInt(configFPS);
-const VIDEO_WIDTH = Number.parseInt(configVideoWidth);
-const VIDEO_HEIGHT = Number.parseInt(configVideoHeight);
-
+// TODO: Pull fact from DB
+// create temp folder with fact name
+// break down text to subtitles: no more than 60 characters, break on , and .!
+// generate audio files with ttstool
+// save them to temp folder
+// call video-renderer
 async function main() {
-  const allAudioFiles = fs.readdirSync('./audio');
+  const db = knex({
+    client: 'pg',
+    connection:
+      'postgresql://kiril:123456@localhost:5432/infobytes?schema=public',
+  });
 
-  const allAudioFilesLengths = await Promise.all(
-    allAudioFiles.map(audioFilePath => {
-      return getAudioFileLength(`./audio/${audioFilePath}`);
-    })
-  );
+  const a = db.queryBuilder();
+  const allFacts = await a.select('*').from('facts');
 
-  const combinedAudioLength = parseFloat(
-    allAudioFilesLengths
-      .reduce(
-        (combinedFilesLength, currentAudioFileLength) =>
-          combinedFilesLength + currentAudioFileLength,
-        0
-      )
-      .toFixed(2)
-  );
-
-  const subtitleTimestampMap = allAudioFilesLengths.reduce<
-    {
-      subtitle: string;
-      startTime: number;
-      endTime: number;
-    }[]
-  >((acc, audioLength, i) => {
-    return [
-      ...acc,
-      {
-        subtitle:
-          i === 0
-            ? 'The Great Wall of China is the longest wall in the world,'
-            : ' stretching over 13,000 miles.',
-        startTime: i === 0 ? 0 : acc[i - 1].endTime,
-        endTime: i === 0 ? audioLength : acc[i - 1].endTime + audioLength,
-      },
-    ];
-  }, []);
-
-  const videoLength = combinedAudioLength;
-  const frameLength = videoLength * FPS;
-
-  for (let i = 0; i < frameLength; i++) {
-    await renderFrame({
-      currentFrame: i,
-      options: {
-        frameOutputDir: OUTPUT_DIR,
-        framesPerSecond: FPS,
-        videoHeight: VIDEO_HEIGHT,
-        videoWidth: VIDEO_WIDTH,
-      },
-      subtitles: subtitleTimestampMap,
-    });
-  }
+  console.log({ allFacts });
 }
 
 main();
