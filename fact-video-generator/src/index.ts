52f7e650e2b627506a16c1567f1aa09bf5c2a4ed
Kiril Dimitrov <kir4agi@gmail.com>
2023-10-31 00:41:49 +0200

    feat: finish index.ts with lots of comments

--- fact-video-generator/src/index.ts
+++ fact-video-generator/src/index.ts
@@ -1,36 +1,78 @@
-import knex from 'knex';
-
 import dotenv from 'dotenv';
 dotenv.config();
 
-// const {
-//   FPS: configFPS,
-//   VIDEO_WIDTH: configVideoWidth,
-//   VIDEO_HEIGHT: configVideoHeight,
-// } = process.env;
+import fs from 'fs';
+import path from 'path';
 
-// const FPS = Number.parseInt(configFPS);
-// const VIDEO_WIDTH = Number.parseInt(configVideoWidth);
-// const VIDEO_HEIGHT = Number.parseInt(configVideoHeight);
-// const FRAMES_OUTPUT_DIR = './frames';
+import db from './modules/db';
+import { queryTts } from './modules/tts-query';
+import {
+  registerAudioSegment,
+  writeAudioSegment,
+} from './modules/audio-segment-writer';
 
-// TODO: Pull fact from DB
-// create temp folder with fact name
-// break down text to subtitles: no more than 60 characters, break on , and .!
-// generate audio files with ttstool
-// save them to temp folder
-// call video-renderer
+import {
+  breakString,
+  buildSubtitleSegments,
+  getAudioDetailsInDir,
+  makeTempFolder,
+} from './utils';
+import { renderVideo } from './modules/video-renderer';
+
+const {
+  FPS: configFPS,
+  VIDEO_WIDTH: configVideoWidth,
+  VIDEO_HEIGHT: configVideoHeight,
+} = process.env;
+
+const FPS = Number.parseInt(configFPS);
+const VIDEO_WIDTH = Number.parseInt(configVideoWidth);
+const VIDEO_HEIGHT = Number.parseInt(configVideoHeight);
+const FRAMES_OUTPUT_DIR = './frames';
+
 async function main() {
-  const db = knex({
-    client: 'pg',
-    connection:
-      'postgresql://kiril:123456@localhost:5432/infobytes?schema=public',
+  console.log('Pulling facts from db...');
+  const allFacts = (await db()).getAllFacts();
+  console.log('Finished pulling facts from db!');
+  const folder = await makeTempFolder({ prefix: 'video-generator-' });
+  console.log(`Generated temp folder: ${folder}`);
+  const factSubstrings = breakString(allFacts[0].text);
+  console.log(`Finished breaking fact to substrings!`);
+  for (let i = 0; i < factSubstrings.length; i++) {
+    console.log(`Querying TTS for fact substring No. ${i + 1}`);
+    const audioBuffer = await queryTts(factSubstrings[i]);
+    console.log(`Finished Querying TTS!`);
+    console.log(`Writing audio segment...`);
+    const writtenAudioFilePath = await writeAudioSegment(
+      folder,
+      audioBuffer,
+      i
+    );
+    console.log(`Finished Writing audio segment!`);
+    registerAudioSegment(folder, writtenAudioFilePath);
+    console.log(`Registered audio segment!`);
+  }
+  console.log(`Getting audio details...`);
+  const { audioFilesDurationsInSeconds, combinedAudioLength } =
+    await getAudioDetailsInDir(folder);
+  console.log(`Finished getting audio details!`);
+  const subtitleSegments = buildSubtitleSegments(
+    factSubstrings,
+    audioFilesDurationsInSeconds
+  );
+  console.log(`Built subtitle segments!`);
+  const framesOutputDir = path.join(folder, FRAMES_OUTPUT_DIR);
+  fs.mkdirSync(framesOutputDir);
+  console.log(`Started rendering frames...`);
+  await renderVideo({
+    framesOutputDir,
+    framesPerSecond: FPS,
+    subtitles: subtitleSegments,
+    videoHeight: VIDEO_HEIGHT,
+    videoWidth: VIDEO_WIDTH,
+    combinedAudioLength,
   });
-
-  const a = db.queryBuilder();
-  const allFacts = await a.select('*').from('facts');
-
-  console.log({ allFacts });
+  console.log(`Finished rendering frames for ${allFacts[0].title}!`);
 }
 
 main();
