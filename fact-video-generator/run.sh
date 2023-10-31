c8af33df8545b0bcb2c6ee39f64e4352289de523
Kiril Dimitrov <kir4agi@gmail.com>
2023-10-31 18:19:36 +0200

    feat: add hooks to run.sh

--- fact-video-generator/run.sh
+++ fact-video-generator/run.sh
@@ -1,12 +1,26 @@
-program_output=$(yarn && yarn build && cp .env ./dist/.env && yarn start)
-audio_path=$(echo "$program_output" | grep "audio:" | awk '{print $2}')
-frames_path=$(echo "$program_output" | grep "video:" | awk '{print $2}')
-output_path=$(echo "$program_output" | grep "output:" | awk '{print $2}')
+# Prepare
+yarn && yarn build && cp .env ./dist/.env
 
-ffmpeg -f concat -safe 0 -i "$audio_path/segments.txt" "$audio_path/generated-audio.mp3"
-ffmpeg -y -framerate 25 -i "$frames_path/frame-%d.png" -i "$audio_path/generated-audio.mp3" -c:v libx264 -pix_fmt yuv420p "$output_path/out.mp4"
+pre_hook_output=$(yarn start:hook:pre | grep "fact-id:" | awk '{print $2}')
 
-# Clean temp folder
-echo "--- Cleaning temp folder $audio_path"
-# rm -rf $audio_path
+while IFS= read -r line; do
+  # Run the command with the current line as the ID
+  program_output=$(yarn start --id=$line 2>&1 | tee /dev/tty)
+  audio_path=$(echo "$program_output" | grep "audio:" | awk '{print $2}')
+  frames_path=$(echo "$program_output" | grep "video:" | awk '{print $2}')
+  output_path=$(echo "$program_output" | grep "output:" | awk '{print $2}')
+
+  ffmpeg -f concat -safe 0 -i "$audio_path/segments.txt" "$audio_path/generated-audio.mp3"
+  ffmpeg -y -framerate 25 -i "$frames_path/frame-%d.png" -i "$audio_path/generated-audio.mp3" -c:v libx264 -pix_fmt yuv420p "$output_path/out.mp4"
+
+  # Clean temp folder
+  echo "--- Cleaning temp folder $audio_path"
+  rm -rf $audio_path
+  
+  yarn start:hook:post
+
+  echo "--- Cleaning output folder $output_path"
+  rm -rf $output_path
+done <<< "$pre_hook_output"
+
 
