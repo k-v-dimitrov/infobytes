929fdc7a8327a0c76b04364ec17ed4235e4b6574
Yordan Hristov <y.hristov11@gmail.com>
2023-11-03 20:27:51 +0200

    Add gluestack ui

--- InfoBytes/App.tsx
+++ InfoBytes/App.tsx
@@ -1,13 +1,17 @@
 import React from "react";
-import {SafeAreaView, StatusBar, useColorScheme} from "react-native";
+import { SafeAreaView, StatusBar, useColorScheme } from "react-native";
+import { config } from "@gluestack-ui/config";
+import { GluestackUIProvider } from "@gluestack-ui/themed";
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === "dark";
 
   return (
-    <SafeAreaView>
-      <StatusBar barStyle={isDarkMode ? "light-content" : "dark-content"} />
-    </SafeAreaView>
+    <GluestackUIProvider config={config}>
+      <SafeAreaView>
+        <StatusBar barStyle={isDarkMode ? "light-content" : "dark-content"} />
+      </SafeAreaView>
+    </GluestackUIProvider>
   );
 }
 
