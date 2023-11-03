c906c464d8af6bcc2f2935af3213f25e5afab5a7
Yordan Hristov <y.hristov11@gmail.com>
2023-11-03 21:47:09 +0200

    Add @react-navigation

--- InfoBytes/App.tsx
+++ InfoBytes/App.tsx
@@ -1,16 +1,74 @@
 import React from "react";
-import { SafeAreaView, StatusBar, useColorScheme } from "react-native";
+import { StatusBar, useColorScheme } from "react-native";
 import { config } from "@gluestack-ui/config";
-import { GluestackUIProvider } from "@gluestack-ui/themed";
+import { GluestackUIProvider, Icon, Text, View } from "@gluestack-ui/themed";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Book, Search, PersonCircle } from "./src/icons";
+
+const Tab = createBottomTabNavigator();
+
+// temporary
+const Screen = () => (
+  <View>
+    <Text>Screen</Text>
+  </View>
+);
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === "dark";
 
   return (
     <GluestackUIProvider config={config}>
-      <SafeAreaView>
-        <StatusBar barStyle={isDarkMode ? "light-content" : "dark-content"} />
-      </SafeAreaView>
+      <StatusBar barStyle={isDarkMode ? "light-content" : "dark-content"} />
+
+      <NavigationContainer>
+        <Tab.Navigator
+          initialRouteName="Feed"
+          screenOptions={{
+            headerShown: false,
+          }}>
+          <Tab.Screen
+            name="Feed"
+            component={Screen}
+            options={{
+              tabBarIcon: ({ focused }) => (
+                <Icon
+                  size="xl"
+                  color={focused ? "$blue500" : "$black"}
+                  as={Book}
+                />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Search"
+            component={Screen}
+            options={{
+              tabBarIcon: ({ focused }) => (
+                <Icon
+                  size="xl"
+                  color={focused ? "$blue500" : "$black"}
+                  as={Search}
+                />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Profile"
+            component={Screen}
+            options={{
+              tabBarIcon: ({ focused }) => (
+                <Icon
+                  size="xl"
+                  color={focused ? "$blue500" : "$black"}
+                  as={PersonCircle}
+                />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
     </GluestackUIProvider>
   );
 }
