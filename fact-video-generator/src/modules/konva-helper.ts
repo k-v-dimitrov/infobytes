20d05cca5de6e10f916047b50cc6ece2e4460000
Kiril Dimitrov <kir4agi@gmail.com>
2023-11-04 10:09:52 +0200

    feat: add konva helper

---
+++ fact-video-generator/src/modules/konva-helper.ts
@@ -0,0 +1,40 @@
+//@ts-nocheck
+import Konva from 'konva';
+import canvas from 'canvas';
+
+// mock window
+Konva.window = {
+  Image: canvas.Image,
+  devicePixelRatio: 1,
+};
+// mock document
+Konva.document = {
+  createElement() {},
+  documentElement: {
+    addEventListener() {},
+  },
+};
+
+// make some global injections
+global.requestAnimationFrame = cb => {
+  setImmediate(cb);
+};
+
+// create canvas in Node env
+Konva.Util.createCanvasElement = () => {
+  const node = new canvas.Canvas();
+  node.style = {};
+  return node;
+};
+
+// create image in Node env
+Konva.Util.createImageElement = () => {
+  const node = new canvas.Image();
+  node.style = {};
+  return node;
+};
+
+// _checkVisibility use dom element, in node we can skip it
+Konva.Stage.prototype._checkVisibility = () => {};
+
+export default Konva;
