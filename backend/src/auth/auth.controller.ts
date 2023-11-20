a89e85752a7d031257a7e9d6a240f23af46f049d
Kiril Dimitrov <kir4agi@gmail.com>
2023-11-20 20:53:19 +0200

    chore: disable request throttler

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -4,7 +4,7 @@ import {
   HttpCode,
   HttpStatus,
   Post,
-  UseGuards,
+  // UseGuards,
 } from '@nestjs/common';
 import {
   LoginDto,
@@ -14,39 +14,39 @@ import {
   ResetPasswordRequestDto,
 } from './dto';
 import { AuthService } from './auth.service';
-import { ThrottlerGuard } from '@nestjs/throttler';
+// import { ThrottlerGuard } from '@nestjs/throttler';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @UseGuards(ThrottlerGuard)
+  // @UseGuards(ThrottlerGuard)
   @Post('register')
   create(@Body() dto: RegisterDto) {
     return this.authService.createUser(dto);
   }
 
-  @UseGuards(ThrottlerGuard)
+  // @UseGuards(ThrottlerGuard)
   @Post('login')
   @HttpCode(HttpStatus.OK)
   login(@Body() dto: LoginDto) {
     return this.authService.login(dto);
   }
 
-  @UseGuards(ThrottlerGuard)
+  // @UseGuards(ThrottlerGuard)
   @Post('reset/request')
   resetPasswordRequest(@Body() dto: ResetPasswordRequestDto) {
     return this.authService.resetPasswordRequest(dto);
   }
 
-  @UseGuards(ThrottlerGuard)
+  // @UseGuards(ThrottlerGuard)
   @Post('reset/check')
   @HttpCode(HttpStatus.OK)
   resetPasswordCheck(@Body() dto: ResetPasswordCheckDto) {
     return this.authService.resetPasswordCheck(dto);
   }
 
-  @UseGuards(ThrottlerGuard)
+  // @UseGuards(ThrottlerGuard)
   @Post('reset')
   @HttpCode(HttpStatus.OK)
   resetPassword(@Body() dto: ResetPasswordDto) {
