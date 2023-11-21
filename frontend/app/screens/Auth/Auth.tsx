8399b48969c4ae9204507576facf16661e206e8a
Yordan Hristov <y.hristov11@gmail.com>
2023-11-21 21:19:21 +0200

    refactor: remove needless code

--- frontend/app/screens/Auth/Auth.tsx
+++ frontend/app/screens/Auth/Auth.tsx
@@ -1,23 +1,18 @@
 import React, { useState } from "react"
 import { Providers, LoginForm, RegisterForm } from "./components"
 import { Screen } from "app/components"
-import { AppStackScreenProps } from "app/navigators"
 
-export const Auth = ({ navigation }) => {
+export const Auth = () => {
   const [isLogin, setIsLogin] = useState(true)
 
   const toggleIsLogin = () => {
     setIsLogin((prev) => !prev)
   }
 
-  const onSubmitSuccess = () => {
-    navigation.navigate(isLogin ? "Feed" : "Onboarding")
-  }
-
   return (
     <Screen justifyContent="space-around">
-      {isLogin && <LoginForm toggleIsLogin={toggleIsLogin} onSubmitSuccess={onSubmitSuccess} />}
-      {!isLogin && <RegisterForm toggleIsLogin={toggleIsLogin} onSubmitSuccess={onSubmitSuccess} />}
+      {isLogin && <LoginForm toggleIsLogin={toggleIsLogin} />}
+      {!isLogin && <RegisterForm toggleIsLogin={toggleIsLogin} />}
 
       <Providers />
     </Screen>
