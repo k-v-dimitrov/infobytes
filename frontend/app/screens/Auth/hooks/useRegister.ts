adcb2188ef6ac582d1a116787da706d500757741
Yordan Hristov <y.hristov11@gmail.com>
2023-11-21 21:20:40 +0200

    feat: register request, loading, error state hook

---
+++ frontend/app/screens/Auth/hooks/useRegister.ts
@@ -0,0 +1,35 @@
+import { useState } from "react"
+import { Credentials, api } from "app/services/api"
+import { saveString } from "app/utils/storage"
+import { useStores } from "app/models"
+import { navigate } from "app/navigators"
+
+export const useRegister = () => {
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<null | string>(null)
+  const { authenticationStore } = useStores()
+
+  const register = async (credentials: Credentials) => {
+    setLoading(true)
+
+    const { error, data } = await api.register(credentials)
+
+    if (error) {
+      setError(error)
+
+      return
+    }
+
+    try {
+      await saveString("userAuthToken", data.token)
+      authenticationStore.authenticate(data.user)
+      navigate({ name: "Feed", params: undefined })
+    } catch (err) {
+      setError("Something went wrong!")
+    }
+
+    setLoading(false)
+  }
+
+  return { register, loading, error }
+}
