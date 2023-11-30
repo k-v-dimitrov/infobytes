f61231318c3d69380cda52edc066d08d3d45f0a0
Kiril Dimitrov <kir4agi@gmail.com>
2023-11-30 12:09:56 +0200

    feat: merge and shuffle questions/facts for feed

--- backend/src/feed/feed.service.ts
+++ backend/src/feed/feed.service.ts
@@ -14,6 +14,7 @@ import {
   InvalidAnswerIdException,
   UserFeedQuestionAlreadyAnsweredException,
 } from './exceptions';
+import { mergeAndShuffleArrays } from 'src/utils/mergeAndShuffleArrays';
 
 // Enrich user feed on every 5 viewed facts
 const ENRICH_WITH_QUESTION_INTERVAL = 5;
@@ -62,7 +63,7 @@ export class FeedService {
       { excludeExtraneousValues: true },
     );
 
-    return feedResponse;
+    return mergeAndShuffleArrays(feedResponse.facts, feedResponse.questions);
   }
 
   async answerFeedQuestion(
