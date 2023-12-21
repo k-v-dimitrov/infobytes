8f8d645d1f11e7250a33cc03bc4e683272ef960c
Kiril Dimitrov <kir4agi@gmail.com>
2023-12-21 14:38:40 +0200

    feat: mark when item is fully in view

--- frontend/app/screens/Feed.tsx
+++ frontend/app/screens/Feed.tsx
@@ -1,10 +1,21 @@
 import React, { Key, useRef, useState, useMemo } from "react"
 import { PanResponder } from "react-native"
-import Animated, { useAnimatedStyle, useSharedValue, withSpring } from "react-native-reanimated"
-import { View, Text } from "@gluestack-ui/themed"
+import Animated, {
+  WithSpringConfig,
+  cancelAnimation,
+  runOnJS,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated"
+import { View, Text, Spinner, CheckIcon } from "@gluestack-ui/themed"
 import { Screen } from "app/components"
 
-const NEXT_ELEMENT_SCROLL_THRESHOLD_PERCENTAGE = 35
+const NEXT_ELEMENT_SCROLL_THRESHOLD_PERCENTAGE = 10
+const SPRING_ANIM_CONFIG: WithSpringConfig = {
+  stiffness: 5,
+  mass: 0.025,
+}
 
 function CustomVirtualizedList<T>({
   data,
@@ -14,23 +25,28 @@ function CustomVirtualizedList<T>({
 }: {
   data: Array<T>
   keyExtractor: (item: T) => Key
-  renderItem: (item: T) => React.ReactNode
+  renderItem: ({ item, isFullyInView }: { item: T; isFullyInView: boolean }) => React.ReactNode
   itemContainerProps?: React.ComponentProps<typeof View>
 }) {
   const [elementHeight, setElementHeight] = useState(0)
-  const [bottomBoundary, setBottomBoundary] = useState(0)
-
   const yOffset = useSharedValue(0)
-
   const animatedStyles = useAnimatedStyle(() => ({
     transform: [{ translateY: yOffset.value }],
   }))
 
+  const calcItemIndexInView = () => Math.abs(Math.round(yOffset.value / elementHeight))
+  const [currentItemIndexInView, setCurrentItemIndexInView] = useState(0)
+
+  const handleScrollAnimationEnd = () => {
+    setCurrentItemIndexInView(calcItemIndexInView())
+  }
+
   const lastDy = useRef(0)
-  const onPanStartIndex = useRef(0)
+  const onPanStartItemIndex = useRef(0)
   const panResponder = useMemo(() => {
     const handleGrant = () => {
-      onPanStartIndex.current = Math.round(yOffset.value / elementHeight)
+      cancelAnimation(yOffset)
+      onPanStartItemIndex.current = Math.round(yOffset.value / elementHeight)
     }
 
     const handleMove = (_, { dy }) => {
@@ -42,24 +58,26 @@ function CustomVirtualizedList<T>({
       lastDy.current = 0
 
       const normalizedOffsetPercentage =
-        ((yOffset.value - elementHeight * onPanStartIndex.current) / elementHeight) * 100
+        ((yOffset.value - elementHeight * onPanStartItemIndex.current) / elementHeight) * 100
 
       const isBackwardScroll = normalizedOffsetPercentage > 0
 
       if (Math.abs(normalizedOffsetPercentage) >= NEXT_ELEMENT_SCROLL_THRESHOLD_PERCENTAGE) {
         const newIndex = isBackwardScroll
-          ? onPanStartIndex.current + 1
-          : onPanStartIndex.current - 1
+          ? onPanStartItemIndex.current + 1
+          : onPanStartItemIndex.current - 1
 
         const isNewIndexInBounds = -newIndex >= 0 && -newIndex <= data.length - 1
 
         if (isNewIndexInBounds) {
-          yOffset.value = withSpring(elementHeight * newIndex)
+          yOffset.value = withSpring(elementHeight * newIndex, SPRING_ANIM_CONFIG, () =>
+            runOnJS(handleScrollAnimationEnd)(),
+          )
           return
         }
       }
 
-      yOffset.value = withSpring(elementHeight * onPanStartIndex.current)
+      yOffset.value = withSpring(elementHeight * onPanStartItemIndex.current, SPRING_ANIM_CONFIG)
     }
 
     return PanResponder.create({
@@ -68,31 +86,26 @@ function CustomVirtualizedList<T>({
       onPanResponderMove: handleMove,
       onPanResponderEnd: handleEnd,
     })
-  }, [elementHeight])
+  }, [elementHeight, handleScrollAnimationEnd])
 
   return (
     <View flex={1} {...panResponder.panHandlers}>
-      {data.map((item) => (
+      {data.map((item, index) => (
         <View key={keyExtractor(item)}>
-          <Animated.View style={[animatedStyles, { height: "100%" }]}>
+          <Animated.View style={animatedStyles}>
             <View
               onLayout={(e) => {
                 const viewHeight = e.nativeEvent.layout.height
                 setElementHeight(viewHeight)
-                setBottomBoundary(viewHeight * data.length)
               }}
               height="$full"
               {...itemContainerProps}
             >
-              {renderItem(item)}
+              {renderItem({ item, isFullyInView: currentItemIndexInView === index })}
             </View>
           </Animated.View>
         </View>
       ))}
-
-      {/* <Animated.View style={[animatedStyles]}>
-        <View height={10} width={10} bgColor="$red100"></View>
-      </Animated.View> */}
     </View>
   )
 }
@@ -103,10 +116,25 @@ export const Feed = () => {
       <CustomVirtualizedList
         data={["test", "test 2", "test 3", "test 4", "test 5"]}
         keyExtractor={(item) => item}
-        renderItem={(item) => {
+        renderItem={({ item, isFullyInView }) => {
           return (
-            <View borderColor="$green100" borderWidth={3}>
+            // This item can have flex={1} and it will be height 100%
+            <View flex={1} borderColor="$green100" borderWidth={1}>
               <Text>{item}</Text>
+              <View
+                flex={1}
+                borderColor="$amber400"
+                borderWidth={1}
+                alignContent="center"
+                justifyContent="center"
+                alignItems="center"
+              >
+                {isFullyInView ? (
+                  <CheckIcon size="xl" color="$green400" />
+                ) : (
+                  <Spinner size={"large"} />
+                )}
+              </View>
             </View>
           )
         }}
