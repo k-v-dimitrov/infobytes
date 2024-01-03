2a5fe64afbe8ade27ef1ce38d1c936d502a410e7
Kiril Dimitrov <kir4agi@gmail.com>
2024-01-03 21:23:24 +0200

    chore: format

--- frontend/app/screens/Profile/Profile.tsx
+++ frontend/app/screens/Profile/Profile.tsx
@@ -1,8 +1,8 @@
+import React from "react"
 import { Button, ButtonText, Divider, Text } from "@gluestack-ui/themed"
 import { observer } from "mobx-react-lite"
 import { Screen } from "app/components"
 import { useStores } from "app/models"
-import React from "react"
 import { ScrollView } from "react-native-gesture-handler"
 
 export const Profile = observer<any>(({ navigation }) => {
