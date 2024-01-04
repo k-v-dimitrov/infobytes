155acb73eb3a7de4af9b610bef21b5a6f4fe1b52
Kiril Dimitrov <kir4agi@gmail.com>
2024-01-04 09:45:36 +0200

    feat: add utility functions for realtime management

--- frontend/app/services/realtime-manager/realtime-manager.tsx
+++ frontend/app/services/realtime-manager/realtime-manager.tsx
@@ -5,6 +5,8 @@ import Config from "../../config"
 import { useStores } from "app/models"
 import { Events } from "./events"
 
+const MAX_ALLOWED_LISTENERS = 10
+
 const useRealtimeManager = () => {
   const { current: socket } = useRef(io(Config.SOCKET_URL, { reconnectionDelay: 10000 }))
 
@@ -13,14 +15,33 @@ const useRealtimeManager = () => {
   } = useStores()
 
   const addRealtimeListener = (event: Events, callback: (payload: unknown) => void) => {
+    const hasReachedMaxListenersCount = socket.listeners(event).length > MAX_ALLOWED_LISTENERS
+
+    if (hasReachedMaxListenersCount) {
+      throw new Error(
+        `Realtime Manager: Reached maximum listeners for event ${event}. Memory leak? `,
+      )
+    }
+
     socket.on(event, (p) => {
       callback(p)
     })
   }
 
+  const removeRealtimeListener = (event: Events, handler: (...args: any[]) => void) => {
+    const hasAnyListener = socket.listeners(event).length > 0
+
+    if (!hasAnyListener) {
+      console.warn(`Trying to remove ${event} which has no listeners!`)
+      return
+    }
+
+    socket.removeListener(event, handler)
+  }
+
   useEffect(() => {
-    socket.on("connect", async () => {
-      await socket.emitWithAck("identity", { jwt: userToken })
+    socket.on("connect", () => {
+      socket.emit("identity", { jwt: userToken })
     })
 
     return () => {
@@ -28,17 +49,19 @@ const useRealtimeManager = () => {
     }
   }, [socket])
 
-  return { addRealtimeListener }
+  return { addRealtimeListener, removeRealtimeListener }
 }
 
 type ContextType = ReturnType<typeof useRealtimeManager>
 const RealtimeContext = createContext<ContextType>(null)
 
 const RealtimeProvider = ({ children }: { children: React.ReactNode }) => {
-  const { addRealtimeListener } = useRealtimeManager()
+  const { addRealtimeListener, removeRealtimeListener } = useRealtimeManager()
 
   return (
-    <RealtimeContext.Provider value={{ addRealtimeListener }}>{children}</RealtimeContext.Provider>
+    <RealtimeContext.Provider value={{ addRealtimeListener, removeRealtimeListener }}>
+      {children}
+    </RealtimeContext.Provider>
   )
 }
 
