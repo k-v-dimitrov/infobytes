cb0beb333df40ae49dde49abf6b5cb1eb34b77bd
Kiril Dimitrov <kir4agi@gmail.com>
2024-01-12 11:55:21 +0200

    feat: install react-native-video and configure it

--- frontend/android/app/src/main/java/com/infobytes/MainApplication.java
+++ frontend/android/app/src/main/java/com/infobytes/MainApplication.java
@@ -12,6 +12,8 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 
+import com.brentvatne.react.ReactVideoPackage;
+
 import java.util.List;
 
 import expo.modules.ApplicationLifecycleDispatcher;
@@ -32,6 +34,7 @@ public class MainApplication extends Application implements ReactApplication {
         List<ReactPackage> packages = new PackageList(this).getPackages();
         // Packages that cannot be autolinked yet can be added manually here, for example:
         // packages.add(new MyReactNativePackage());
+        packages.add(new ReactVideoPackage());
         return packages;
       }
 
