863c49678dcba13288d883d1e9e0cbe5f3fcaa18
Kiril Dimitrov <kir4agi@gmail.com>
2024-01-13 02:11:24 +0200

    feat: extend useFeedManager with utility functions

--- frontend/app/screens/Feed/useFeedManager.ts
+++ frontend/app/screens/Feed/useFeedManager.ts
@@ -3,6 +3,8 @@ import { feedApi } from "app/services/api/feed"
 import { autorun } from "mobx"
 import { useEffect, useState } from "react"
 
+const ITEMS_TO_PRELOAD = 2
+
 const useFeedManager = () => {
   const { authenticationStore } = useStores()
   const [feedList, setFeedList] = useState([])
@@ -28,10 +30,47 @@ const useFeedManager = () => {
 
   useEffect(() => {
     autorun(() => {
-      if (authenticationStore.feedUserId) {
+      if (authenticationStore.feedUserId && feedList.length === 0) {
+        pullNextFeedChunk(authenticationStore.feedUserId)
       }
     })
-  }, [])
+  }, [feedList])
+
+  // TODO: when needed implement this function
+  // This will not work because it will shift the indecies of the items and will cause TikTok list to break
+
+  // const freeFeedItemsFromMemory = (threshold = 50, itemsToRemove = 10) => {
+  //   if (feedList.length >= threshold) {
+  //     setFeedList((prev) => prev.slice(undefined, itemsToRemove))
+  //   }
+  // }
+
+  const pullNextFeedChunk = async (feedUserId: string) => {
+    // freeFeedItemsFromMemory()
+    const { data } = await feedApi.getUserFeed(feedUserId)
+    setFeedList((prev) => [...prev, ...data])
+  }
+
+  const shouldPullNextFeedChunk = (currentViewedIndex: number) => {
+    if (currentViewedIndex === feedList.length - 1) {
+      return true
+    }
+
+    return false
+  }
+
+  const shouldRenderFeedItem = (
+    currentlyRenderedItemIndex: number,
+    currentlyViewedItemIndex: number,
+  ) => {
+    if (Math.abs(currentlyViewedItemIndex - currentlyRenderedItemIndex) < ITEMS_TO_PRELOAD) {
+      return true
+    }
+
+    return false
+  }
+
+  return { feedList, pullNextFeedChunk, shouldPullNextFeedChunk, shouldRenderFeedItem }
 }
 
 export default useFeedManager
