3d2789880c15a9667befbf40e1c992a90edcc53d
Kiril Dimitrov <kir4agi@gmail.com>
2024-01-15 23:10:16 +0200

    feat: restructure feed

--- frontend/app/screens/Feed/Feed.tsx
+++ frontend/app/screens/Feed/Feed.tsx
@@ -1,10 +1,12 @@
 import React, { ComponentRef, useRef } from "react"
+import { Text } from "react-native"
 import { observer } from "mobx-react-lite"
 import { Screen } from "app/components"
 import { TikTokList } from "./TiktokList"
 import { VideoPlayer } from "./VideoPlayer"
 import { useStores } from "app/models"
 import useFeedManager from "./useFeedManager"
+import { isFeedFact, isFeedQuestion, FeedItem } from "app/services/api/feed"
 
 export const Feed = observer<any>(() => {
   const { feedList, pullNextFeedChunk, shouldPullNextFeedChunk } = useFeedManager()
@@ -20,6 +22,63 @@ export const Feed = observer<any>(() => {
     currentItemIndexInView.current = itemIndex
   }
 
+  const extractKeyFromFeedItem = (feedItem: FeedItem) => {
+    if (isFeedFact(feedItem)) {
+      const feedFact = feedItem
+      return feedFact.id
+    }
+
+    if (isFeedQuestion(feedItem)) {
+      const feedQuestion = feedItem
+      return feedQuestion.data.id
+    }
+
+    console.warn("Unknown feed item was encountered:", feedItem)
+
+    return null
+  }
+
+  const renderFeedItem = ({
+    item,
+    index,
+    isFullyInView,
+  }: {
+    item: FeedItem
+    index: number
+    isFullyInView: boolean
+  }) => {
+    // TODO: check calculation of currentItemIndexInView
+    // if (!shouldRenderFeedItem(index, currentItemIndexInView.current)) {
+    //   return null
+    // }
+
+    if (isFullyInView) {
+      if (isFeedFact(item)) {
+        const feedFact = item
+
+        return (
+          <VideoPlayer
+            onEnd={() => {
+              if (listRef.current) {
+                listRef.current.playInviteToNextItemAnimation()
+              }
+            }}
+            factId={feedFact.id}
+            play
+          />
+        )
+      }
+
+      if (isFeedQuestion(item)) {
+        return <Text>Feed Qustion!</Text>
+      }
+
+      console.warn("Uknown feed item was encountered:", item)
+    }
+
+    return null
+  }
+
   return (
     <Screen p="$0">
       <TikTokList
@@ -27,25 +86,8 @@ export const Feed = observer<any>(() => {
           if (ref) listRef.current = ref
         }}
         data={feedList}
-        keyExtractor={({ id }) => id}
-        renderItem={({ item, index, isFullyInView }) => {
-          // TODO: check calculation of currentItemIndexInView
-          // if (!shouldRenderFeedItem(index, currentItemIndexInView.current)) {
-          //   return null
-          // }
-          if (isFullyInView)
-            return (
-              <VideoPlayer
-                onEnd={() => {
-                  if (listRef.current) {
-                    listRef.current.playInviteToNextItemAnimation()
-                  }
-                }}
-                factId={item.id}
-                play={isFullyInView}
-              />
-            )
-        }}
+        keyExtractor={extractKeyFromFeedItem}
+        renderItem={renderFeedItem}
         itemContainerProps={{ bgColor: "$blueGray800" }}
         onCurrentItemInViewChange={handleCurrentItemInViewChange}
       />
