6b4d5de60fcfa58eafb1efe51579756476141268
Kiril Dimitrov <kir4agi@gmail.com>
2024-01-15 21:35:37 +0200

    fix: rebase issues

--- frontend/app/services/api/feed/feed.ts
+++ frontend/app/services/api/feed/feed.ts
@@ -3,17 +3,7 @@ import { FeedFact, SubscribeUserToFeed } from "./feed.types"
 
 class FeedApi extends Api {
   async subscribeUserToFeed() {
-    const authToken = await this.getAuthToken()
-
-    const { data, ok, status } = await this.apisauce.get<SubscribeUserToFeed>(
-      "feed/user",
-      {},
-      {
-        headers: {
-          Authorization: `Bearer ${authToken}`,
-        },
-      },
-    )
+    const { data, ok, status } = await this.protectedApisauce.get<SubscribeUserToFeed>("feed/user")
 
     return {
       data,
@@ -22,18 +12,11 @@ class FeedApi extends Api {
   }
 
   async getUserFeed(userFeedId: string) {
-    const authToken = await this.getAuthToken()
-
     // TODO: handle feed questions
-    const { data, ok, status } = await this.apisauce.get<FeedFact[]>(
-      "/feed",
-      { userFeedId, size: 2 },
-      {
-        headers: {
-          Authorization: `Bearer ${authToken}`,
-        },
-      },
-    )
+    const { data, ok, status } = await this.protectedApisauce.get<FeedFact[]>("/feed", {
+      userFeedId,
+      size: 2,
+    })
 
     return {
       data,
