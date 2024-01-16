2b50ea504af7319db2a217924f09b5f3e0697154
Kiril Dimitrov <kir4agi@gmail.com>
2024-01-16 23:10:17 +0200

    feat: answer and display correct/wrong choices

--- frontend/app/screens/Feed/components/QuizButton.tsx
+++ frontend/app/screens/Feed/components/QuizButton.tsx
@@ -1,22 +1,31 @@
 import React from "react"
-import { Button, ButtonText } from "@gluestack-ui/themed"
+import { Button, ButtonText, Spinner } from "@gluestack-ui/themed"
 
 interface QuizButtonProps {
   text: string
   onClick?: () => void
+  isCorrect?: boolean
+  isWrong?: boolean
+  isLoading?: boolean
 }
 
-export const QuizButton = ({ text, onClick }: QuizButtonProps) => (
-  <Button
-    w={250}
-    height="$24"
-    size="lg"
-    variant="outline"
-    action="primary"
-    isDisabled={false}
-    isFocusVisible={false}
-    onPress={onClick}
-  >
-    <ButtonText>{text}</ButtonText>
-  </Button>
-)
+export const QuizButton = ({ text, isCorrect, isWrong, isLoading, onClick }: QuizButtonProps) => {
+  const borderColor = isCorrect ? "$green500" : isWrong ? "$red500" : "$blue900"
+
+  return (
+    <Button
+      w={250}
+      height="$24"
+      size="lg"
+      variant="outline"
+      action="primary"
+      isDisabled={false}
+      isFocusVisible={false}
+      onPressOut={onClick}
+      borderColor={borderColor}
+      borderWidth={3}
+    >
+      {isLoading ? <Spinner /> : <ButtonText>{text}</ButtonText>}
+    </Button>
+  )
+}
--- frontend/app/screens/Feed/components/RenderFeedQuestion.tsx
+++ frontend/app/screens/Feed/components/RenderFeedQuestion.tsx
@@ -3,11 +3,18 @@ import React, { useState, useEffect } from "react"
 import { View, VStack, Heading } from "@gluestack-ui/themed"
 import LottieView from "lottie-react-native"
 
-import { FeedQuestion } from "app/services/api/feed"
+import {
+  Answer,
+  FeedQuestion,
+  feedApi,
+  isCorrectAnswerResponse,
+  isWrongAnswerResponse,
+} from "app/services/api/feed"
 
 import { QuizButton } from "./QuizButton"
 
 import QuizAppearAnim from "../../../../assets/lottie/quiz-appear.json"
+import { useApi } from "app/hooks"
 
 export const RenderFeedQuestion = ({
   question,
@@ -17,7 +24,49 @@ export const RenderFeedQuestion = ({
   isFullyInView: boolean
 }) => {
   const [animFinished, setAnimFinished] = useState(false)
+  const [selectedAnswer, setSelectedAnswer] = useState<Answer | null>(null)
+  const [correctAnswerId, setCorrectAnswerId] = useState(null)
 
+  const {
+    trigger: answerQuestion,
+    error,
+    loading,
+  } = useApi(feedApi.answerFeedQuestion, {
+    executeOnMount: false,
+    props: [{ answerId: selectedAnswer?.id, questionUri: question.data.answerURI }],
+    onSuccess: (answerResponse) => {
+      if (isCorrectAnswerResponse(answerResponse)) {
+        setCorrectAnswerId(selectedAnswer.id)
+      }
+
+      if (isWrongAnswerResponse(answerResponse)) {
+        setCorrectAnswerId(answerResponse.correctAnswerId)
+      }
+
+      if (!isCorrectAnswerResponse(answerResponse) && !isWrongAnswerResponse(answerResponse)) {
+        console.warn("Unknown data for answer response: ", answerResponse)
+      }
+    },
+    onError: () => {
+      // TODO: Show error to user
+      console.warn(error)
+    },
+  })
+
+  const handleSelectedAnswer = (answer: Answer) => () => {
+    if (!selectedAnswer) {
+      setSelectedAnswer(answer)
+    }
+  }
+
+  // Trigger api call
+  useEffect(() => {
+    if (selectedAnswer) {
+      answerQuestion()
+    }
+  }, [selectedAnswer])
+
+  // Quiz animation useEffect
   useEffect(() => {
     let tId = null
 
@@ -44,7 +93,18 @@ export const RenderFeedQuestion = ({
 
           <VStack gap="$2" flex={1}>
             {question.data.answers.map((answer) => (
-              <QuizButton key={answer.id} text={answer.text} />
+              <QuizButton
+                key={answer.id}
+                text={answer.text}
+                onClick={handleSelectedAnswer(answer)}
+                isWrong={
+                  correctAnswerId &&
+                  selectedAnswer.id === answer.id &&
+                  answer.id !== correctAnswerId
+                }
+                isCorrect={correctAnswerId && answer.id === correctAnswerId}
+                isLoading={loading}
+              />
             ))}
           </VStack>
         </>
