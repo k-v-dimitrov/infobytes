56a0de5763cdc4f19b75d2421c8597731c7ad267
Kiril Dimitrov <kir4agi@gmail.com>
2024-01-18 21:11:08 +0200

    feat: update prop type name,

--- frontend/app/screens/Feed/components/QuizButton.tsx
+++ frontend/app/screens/Feed/components/QuizButton.tsx
@@ -3,13 +3,13 @@ import { Button, ButtonText, Spinner } from "@gluestack-ui/themed"
 
 interface QuizButtonProps {
   text: string
-  onClick?: () => void
+  onPress?: () => void
   isCorrect?: boolean
   isWrong?: boolean
   isLoading?: boolean
 }
 
-export const QuizButton = ({ text, isCorrect, isWrong, isLoading, onClick }: QuizButtonProps) => {
+export const QuizButton = ({ text, isCorrect, isWrong, isLoading, onPress }: QuizButtonProps) => {
   const borderColor = isCorrect ? "$green500" : isWrong ? "$red500" : "$blue900"
 
   return (
@@ -21,7 +21,7 @@ export const QuizButton = ({ text, isCorrect, isWrong, isLoading, onClick }: Qui
       action="primary"
       isDisabled={false}
       isFocusVisible={false}
-      onPressOut={onClick}
+      onPress={onPress}
       borderColor={borderColor}
       borderWidth={3}
     >
