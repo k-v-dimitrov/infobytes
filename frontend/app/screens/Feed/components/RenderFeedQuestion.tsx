6c7722c7dc5a037b31c76de2c40ea6366c02b6f2
Kiril Dimitrov <kir4agi@gmail.com>
2024-01-19 22:00:18 +0200

    feat: play good job animation on answered correctly

--- frontend/app/screens/Feed/components/RenderFeedQuestion.tsx
+++ frontend/app/screens/Feed/components/RenderFeedQuestion.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-native/no-inline-styles */
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useRef } from "react"
 import { View, VStack, Heading } from "@gluestack-ui/themed"
 import LottieView from "lottie-react-native"
 
@@ -14,7 +14,9 @@ import { useApi } from "app/hooks"
 
 import { TikTokListRef } from "../TiktokList"
 import { QuizButton } from "./QuizButton"
+
 import QuizAppearAnim from "../../../../assets/lottie/quiz-appear.json"
+import GoodJobAnim from "../../../../assets/lottie/good-job.json"
 
 interface RenderFeedQuestonProps {
   question: FeedQuestion
@@ -29,9 +31,12 @@ export const RenderFeedQuestion = ({
   topInset = 0,
   listRef,
 }: RenderFeedQuestonProps) => {
-  const [animFinished, setAnimFinished] = useState(false)
+  const [appearAnimFinished, setAppearAnimFinished] = useState(false)
+  const [exitAnimationProgress, setExitAnimationProgress] = useState(0)
   const [selectedAnswer, setSelectedAnswer] = useState<Answer | null>(null)
   const [correctAnswerId, setCorrectAnswerId] = useState(null)
+  const [userAnswerResult, setUserAnswerResult] = useState<{ isCorrect: boolean } | null>(null)
+  const calledOnEnd = useRef(false)
 
   const {
     trigger: answerQuestion,
@@ -42,20 +47,18 @@ export const RenderFeedQuestion = ({
     props: [{ answerId: selectedAnswer?.id, questionUri: question.data.answerURI }],
     onSuccess: (answerResponse) => {
       if (isCorrectAnswerResponse(answerResponse)) {
+        setUserAnswerResult({ isCorrect: true })
         setCorrectAnswerId(selectedAnswer.id)
       }
 
       if (isWrongAnswerResponse(answerResponse)) {
+        setUserAnswerResult({ isCorrect: false })
         setCorrectAnswerId(answerResponse.correctAnswerId)
       }
 
       if (!isCorrectAnswerResponse(answerResponse) && !isWrongAnswerResponse(answerResponse)) {
         console.warn("Unknown data for answer response: ", answerResponse)
       }
-
-      setTimeout(() => {
-        listRef.current.advanceItem()
-      }, 500)
     },
     onError: () => {
       // TODO: Show error to user
@@ -80,16 +83,33 @@ export const RenderFeedQuestion = ({
   useEffect(() => {
     let tId = null
 
-    if (isFullyInView && !animFinished) {
-      tId = setTimeout(() => setAnimFinished(true), 1000)
+    if (isFullyInView && !appearAnimFinished) {
+      tId = setTimeout(() => setAppearAnimFinished(true), 1000)
     }
 
     return () => clearTimeout(tId)
   }, [isFullyInView])
 
+  // Exit Animation Controller
+  useEffect(() => {
+    let timer: string | number | NodeJS.Timeout
+    if (userAnswerResult && userAnswerResult?.isCorrect) {
+      timer = setInterval(() => {
+        if (exitAnimationProgress >= 0.8 && !calledOnEnd.current) {
+          listRef.current.advanceItem()
+          calledOnEnd.current = true
+        }
+
+        setExitAnimationProgress((prevProgress) => prevProgress + 0.01)
+      }, 5)
+    }
+
+    return () => clearInterval(timer)
+  }, [userAnswerResult, exitAnimationProgress, setExitAnimationProgress])
+
   return (
     <View marginTop={topInset} px="$10" alignItems="center" flex={1} gap="$8">
-      {!animFinished ? (
+      {!appearAnimFinished ? (
         <View flex={1} width="$full">
           <LottieView resizeMode="contain" source={QuizAppearAnim} autoPlay style={{ flex: 1 }} />
         </View>
@@ -119,6 +139,28 @@ export const RenderFeedQuestion = ({
           </VStack>
         </>
       )}
+      {exitAnimationProgress ? (
+        <View
+          position="absolute"
+          top={0}
+          left={0}
+          right={0}
+          bottom={0}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <View flex={1} width="$full">
+            <LottieView
+              resizeMode="contain"
+              source={GoodJobAnim}
+              colorFilters={[{ keypath: "GOOD JOB! Outlines", color: "white" }]}
+              style={{ flex: 1 }}
+              autoPlay
+              progress={exitAnimationProgress}
+            />
+          </View>
+        </View>
+      ) : null}
     </View>
   )
 }
