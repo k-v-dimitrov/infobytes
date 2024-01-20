977a4e0d257ec46d08b2ad9db00e6a10bdbcbf22
Kiril Dimitrov <kir4agi@gmail.com>
2024-01-20 12:39:16 +0200

    fix: android release build

--- frontend/App.js
+++ frontend/App.js
@@ -1,19 +1,31 @@
-// This is the entry point if you run `yarn expo:start`
-// If you run `yarn ios` or `yarn android`, it'll use ./index.js instead.
+// // This is the entry point if you run `yarn expo:start`
+// // If you run `yarn ios` or `yarn android`, it'll use ./index.js instead.
+// import App from "./app/app.tsx"
+// import React from "react"
+// import { registerRootComponent } from "expo"
+// import { Platform } from "react-native"
+// import * as SplashScreen from "expo-splash-screen"
+
+// SplashScreen.preventAutoHideAsync()
+
+// function IgniteApp() {
+//   return <App hideSplashScreen={SplashScreen.hideAsync} />
+// }
+
+// if (Platform.OS !== "web") {
+//   registerRootComponent(IgniteApp)
+// }
+
+// export default IgniteApp
+
 import App from "./app/app.tsx"
 import React from "react"
-import { registerRootComponent } from "expo"
-import { Platform } from "react-native"
-import * as SplashScreen from "expo-splash-screen"
-
-SplashScreen.preventAutoHideAsync()
+import { AppRegistry } from "react-native"
+import RNBootSplash from "react-native-bootsplash"
 
 function IgniteApp() {
-  return <App hideSplashScreen={SplashScreen.hideAsync} />
+  return <App hideSplashScreen={RNBootSplash.hide} />
 }
 
-if (Platform.OS !== "web") {
-  registerRootComponent(IgniteApp)
-}
-
-export default IgniteApp
+AppRegistry.registerComponent("InfoBytes", () => IgniteApp)
+export default App
